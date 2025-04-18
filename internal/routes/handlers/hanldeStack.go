d03dd24806a7f1f5b005103aaf446594347f6784
Demetrius <25155822+Laelapa@users.noreply.github.com>
2025-04-18 19:47:40 +0300

    Point canonical domain entries to new domain.

--- internal/routes/handlers/hanldeStack.go
+++ internal/routes/handlers/hanldeStack.go
@@ -10,7 +10,7 @@ func (h *Handler) HandleGetStack(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 
 	// TODO: pull the parameters from env/secrets & request to pass them to the template instead of hardcoding them
-	if err := templates.Stack("Laelapa - Demetrius Papas - Tech Stack", "laelapa.fly.dev", "/stack").Render(r.Context(), w); err != nil {
+	if err := templates.Stack("Laelapa - Demetrius Papas - Tech Stack", "laelapa.dev", "/stack").Render(r.Context(), w); err != nil {
 		h.LogError("Failed to render stack page", r, err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
